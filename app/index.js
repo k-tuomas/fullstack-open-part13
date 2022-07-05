05dbd001e901f7cbcd4c0a50c05c053660385637
k-tuomas <karatuomas@gmail.com>
2022-07-05 16:04:15 +0300

    started on 13.4

--- app/index.js
+++ app/index.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const cors = require('cors');
-const { Sequelize, Model, DataTypes } = require('sequelize')
 require('dotenv').config()
 
 const blogsRouter = require('./routes/blogs')
 
 const app = express()
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-})
 
 app.use(cors())
 app.use(express.json())
@@ -24,3 +14,4 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
